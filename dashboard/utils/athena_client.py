a5212f55dd1a634023433af182861ca0d54d6cc6
Ubuntu <ubuntu@ip-172-31-8-205.ap-south-1.compute.internal>
2026-05-25 23:05:26 +0000

    Fixed AWS region configuration for Streamlit container

--- dashboard/utils/athena_client.py
+++ dashboard/utils/athena_client.py
@@ -1,20 +1,18 @@
 import awswrangler as wr
-import pandas as pd
-
-
-DATABASE = "digital_payments_analytics"
-
-ATHENA_OUTPUT = (
-    "s3://digital-payments-kiran/athena-query-results/"
-)
+import boto3
+import os
 
 
 def run_query(query):
 
+    session = boto3.Session(
+        region_name=os.getenv("AWS_REGION", "us-east-1")
+    )
+
     df = wr.athena.read_sql_query(
         sql=query,
-        database=DATABASE,
-        s3_output=ATHENA_OUTPUT
+        database="your_database_name",
+        boto3_session=session
     )
 
     return df
