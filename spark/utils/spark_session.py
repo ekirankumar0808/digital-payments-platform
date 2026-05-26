5bbc64a86c49579f11da06df70bc5996c4689cb9
Ubuntu <ubuntu@ip-172-31-8-205.ap-south-1.compute.internal>
2026-05-26 22:46:32 +0000

    updated the spark dependencies

--- spark/utils/spark_session.py
+++ spark/utils/spark_session.py
@@ -1,13 +1,13 @@
 from pyspark.sql import SparkSession
-import os
 
 
-def create_spark_session(app_name: str) -> SparkSession:
+def create_spark_session(app_name: str):
 
     spark = (
         SparkSession.builder
         .appName(app_name)
 
+        # Delta
         .config(
             "spark.sql.extensions",
             "io.delta.sql.DeltaSparkSessionExtension"
@@ -26,29 +26,17 @@ def create_spark_session(app_name: str) -> SparkSession:
 
         .config(
             "spark.hadoop.fs.s3a.aws.credentials.provider",
-            "com.amazonaws.auth.EnvironmentVariableCredentialsProvider"
+            "com.amazonaws.auth.DefaultAWSCredentialsProviderChain"
         )
 
-        .config(
-            "spark.hadoop.fs.s3a.access.key",
-            os.getenv("AWS_ACCESS_KEY_ID")
-        )
-
-        .config(
-            "spark.hadoop.fs.s3a.secret.key",
-            os.getenv("AWS_SECRET_ACCESS_KEY")
-        )
-
-        .config(
-            "spark.hadoop.fs.s3a.endpoint",
-            "s3.ap-south-1.amazonaws.com"
-        )
-
+        # Performance
         .config("spark.driver.memory", "512m")
         .config("spark.executor.memory", "512m")
-        .config("spark.sql.shuffle.partitions", "2")
+        .config("spark.sql.shuffle.partitions", "4")
 
         .getOrCreate()
     )
+
+    spark.sparkContext.setLogLevel("WARN")
 
     return spark
